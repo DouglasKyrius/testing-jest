6a45ff54fa0fe466c5b64882810fec431218ddeb
DouglasKyrius <douglaskyrius@gmail.com>
2020-06-13 18:49:07 -0400

    uploading inital files to the repo

---
+++ dataBase/googleDatabase.js
@@ -0,0 +1,11 @@
+const googleDatabase = [
+  'cats.com',
+  'souprecipes.com',
+  'flowers.com',
+  'animals.com',
+  'catpictures.com',
+  'myfavouritecats.com',
+  'morecats.com'
+];
+
+module.exports = googleDatabase;
